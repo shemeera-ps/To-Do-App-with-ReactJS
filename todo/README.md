fa68fbee9cd27eb9753921618c82f0f6ed7ec099
shemeera-ps <shemeeraps99@gmail.com>
2023-09-05 12:32:12 +0530

    todo

---
+++ todo/README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <img src="public/screenshots/todoReact.png">
+</p>
---
+++ todo/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>ToDo List</h1>
+    </div>
+  );
+};
+export default Header;
